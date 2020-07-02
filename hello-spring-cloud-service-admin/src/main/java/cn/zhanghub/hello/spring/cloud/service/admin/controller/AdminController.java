10599964d481ed930eba142f8f5b6ca04d5723d7
zhangwenjun <njauzhangwenjun@163.com>
2020-07-02 21:57:09 +0800

    init service-admin

---
+++ hello-spring-cloud-service-admin/src/main/java/cn/zhanghub/hello/spring/cloud/service/admin/controller/AdminController.java
@@ -0,0 +1,11 @@
+package cn.zhanghub.hello.spring.cloud.service.admin.controller;
+
+/**
+ * @Description: TODO
+ * @author 张文军
+ * @Company: njauit.cn
+ * @version: 1.0
+ * @date 2020/7/221:34
+ */
+public class AdminController {
+}
