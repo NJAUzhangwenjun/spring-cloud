4d6bacf5b311d6a4261fe7acc2e55dd008f7c8c7
zhangwenjun <njauzhangwenjun@163.com>
2020-07-02 22:15:31 +0800

    init web-admin-ribbon

---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/cn/zhanghub/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -0,0 +1,11 @@
+package cn.zhanghub.hello.spring.cloud.web.admin.ribbon;
+
+/**
+ * @Description: TODO
+ * @author 张文军
+ * @Company: njauit.cn
+ * @version: 1.0
+ * @date 2020/7/222:09
+ */
+public class WebAdminRibbonApplication {
+}
